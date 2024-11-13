fb3fcabb306025eecab5c2ac6e82ecd288dae51c
reactoholic <petar.georgiev.kolev@gmail.com>
2024-11-13 02:04:23 +0200

    add theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,53 @@
-import { Link, Paper, Typography, useTheme, useMediaQuery } from '@mui/material'
+'use client'
 
-export default function Home() {
-  const theme = useTheme()
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
-  console.log('isSmallScreen', isSmallScreen)
+import React from 'react'
 
+import {
+  THEME_ID,
+  createTheme as muiCreateTheme,
+  ThemeProvider as MaterialThemeProvider
+} from '@mui/material/styles'
+import { Link, Paper, Typography } from '@mui/material'
+
+const materialTheme = muiCreateTheme({
+  palette: {
+    primary: {
+      main: '#4A90E2'
+    },
+
+    text: {
+      primary: '#fff'
+    },
+
+    background: {
+      default: '#333'
+    }
+  }
+})
+
+export default function App() {
   return (
-    <Paper
-      sx={{
-        display: 'grid',
-        placeItems: 'center',
+    <MaterialThemeProvider theme={{ [THEME_ID]: materialTheme }}>
+      <Paper
+        sx={(theme) => ({
+          display: 'grid',
+          placeItems: 'center',
 
-        width: '100vw',
-        height: '100vh',
-        textWrap: 'pretty',
-        padding: 'clamp(1rem, 5vw, 2rem)',
-        fontSize: 'clamp(1.25rem, 5vw, 2rem)'
-      }}
-    >
-      <Typography variant={isSmallScreen ? 'body1' : 'h1'}>
-        Welcome to the OCD developer guidelines!{' '}
-        <Link href="#" underline="always">
-          You&apos;re not alone.
-        </Link>
-      </Typography>
-    </Paper>
+          width: '100vw',
+          height: '100vh',
+          textWrap: 'pretty',
+          padding: 'clamp(1rem, 5vw, 2rem)',
+          backgroundColor: theme.palette.background.default
+        })}
+      >
+        <Typography fontFamily="monospace" fontSize={'clamp(1.25rem, 5vw, 3.5rem)'}>
+          Welcome to the OCD developer guidelines!
+          <br />
+          <Link href="/" underline="always">
+            You&apos;re not alone.
+          </Link>
+        </Typography>
+      </Paper>
+    </MaterialThemeProvider>
   )
 }
