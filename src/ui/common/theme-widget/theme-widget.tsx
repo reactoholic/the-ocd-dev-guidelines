8b2c52b6ad5eb75d9512618977f29396795ee9d2
reactoholic <petar.georgiev.kolev@gmail.com>
2024-11-18 18:29:01 +0200

    update theme switch method

--- src/ui/common/theme-widget/theme-widget.tsx
+++ src/ui/common/theme-widget/theme-widget.tsx
@@ -5,11 +5,11 @@ import { useState } from 'react'
 import { InputSwitch } from 'primereact/inputswitch'
 
 export const ThemeWidget = ({ className }: { className?: string }) => {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(false) // TODO: Замени useState с useLocalStorage от библиотеката с куките!
 
   return (
     <div className={className}>
-      <InputSwitch checked={isDark} onChange={() => setIsDark(!isDark)} />
+      <InputSwitch checked={isDark} onChange={(e) => setIsDark(e.value)} />
     </div>
   )
 }
