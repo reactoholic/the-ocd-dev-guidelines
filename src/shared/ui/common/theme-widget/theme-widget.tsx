52d2a241d051272544a5330333021c1db0329dbd
reactoholic <petar.georgiev.kolev@gmail.com>
2024-11-21 00:24:30 +0200

    wip - theme widget

--- src/shared/ui/common/theme-widget/theme-widget.tsx
+++ src/shared/ui/common/theme-widget/theme-widget.tsx
@@ -11,12 +11,20 @@ import classes from './theme-widget.module.scss'
 export const ThemeWidget = ({ className }: ThemeWidgetProps) => {
   const [theme, setTheme] = useLocalStorage<Theme>(constants.localStorageKeys.theme, Theme['LIGHT'])
 
+  const isDark = theme === Theme['DARK']
+
   return (
-    <div className={className}>
-      <InputSwitch
-        checked={theme === Theme['DARK']}
-        onChange={(e) => setTheme(e.value ? Theme['DARK'] : Theme['LIGHT'])}
-      />
+    <div className={clsx(classes['theme-widget'], className)}>
+      <div className={classes['toggle-wrapper']}>
+        <i className={clsx('pi pi-sun', classes.icon, !isDark && classes.sun)} />
+
+        <InputSwitch
+          checked={isDark}
+          onChange={(e) => setTheme(e.value ? Theme['DARK'] : Theme['LIGHT'])}
+        />
+
+        <i className={clsx('pi pi-moon', classes.icon, isDark && classes.moon)} />
+      </div>
 
       <i className={clsx('pi pi-github', classes.icon)} />
     </div>
